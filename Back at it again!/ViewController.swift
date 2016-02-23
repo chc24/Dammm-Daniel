05e42606faf02be3cfd573986f0e256f0a3e6f66
Calvin Chueh <chchueh24@gmail.com>
2016-02-23 03:02:49 -0500

    MVP

--- Back at it again!/ViewController.swift
+++ Back at it again!/ViewController.swift
@@ -12,19 +12,54 @@ import AVFoundation
 class ViewController: UIViewController {
 
     var audioPlayer = AVAudioPlayer()
-    var audioRecorder = AVAudioRecorder()
-    let fileName = "demo.caf"
+    var audioRecorder: AVAudioRecorder!
     
     @IBOutlet weak var damn1: UIButton!
     @IBOutlet weak var damn2: UIButton!
     @IBOutlet weak var damn3: UIButton!
     @IBOutlet weak var full: UIButton!
     @IBOutlet weak var stop: UIButton!
+    @IBOutlet weak var record: UIButton!
     
-    @IBOutlet weak var recordButton: UIButton!
+    var isRecording = true
+    var nameRecorded = false
     
     @IBAction func recordSound(sender: UIButton) {
-        
+        print("clicked")
+        if nameRecorded {
+            if (!audioRecorder.recording){
+                do {
+                    try audioPlayer = AVAudioPlayer(contentsOfURL: audioRecorder.url)
+                    audioPlayer.play()
+                } catch {
+                }
+            }
+        }
+        else if isRecording {
+            print("isrecording")
+            record.setTitle("Stop Record", forState: UIControlState.Normal)
+            isRecording = false
+            if !audioRecorder.recording {
+                let audioSession = AVAudioSession.sharedInstance()
+                do {
+                    try audioSession.setActive(true)
+                    audioRecorder.record()
+                } catch {
+                }
+            }
+        } else {
+            record.setTitle("Play Name", forState: UIControlState.Normal)
+            isRecording = true
+            audioRecorder.stop()
+            nameRecorded = true
+            let audioSession = AVAudioSession.sharedInstance()
+            do {
+                try audioSession.overrideOutputAudioPort(AVAudioSessionPortOverride.Speaker)
+                try audioSession.setActive(false)
+            } catch {
+            }
+
+        }
     }
     
     @IBAction func stopSound(sender: AnyObject) {
@@ -56,29 +91,55 @@ class ViewController: UIViewController {
         
         audioPlayer.currentTime = 0.0
         audioPlayer.prepareToPlay()
+        audioPlayer.volume = 1
         audioPlayer.play()
     }
     
+    let recordSettings = [AVSampleRateKey : NSNumber(float: Float(44100.0)),
+        AVFormatIDKey : NSNumber(int: Int32(kAudioFormatMPEG4AAC)),
+        AVNumberOfChannelsKey : NSNumber(int: 1),
+        AVEncoderAudioQualityKey : NSNumber(int: Int32(AVAudioQuality.Medium.rawValue))]
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //self.view.backgroundColor = UIColor(patternImage: UIImage(named: "white_van.jpg")!)
+        self.title = "DAMNNNN"
+        
         var buttons = [UIButton!]()
         buttons.append(damn1)
         buttons.append(damn2)
         buttons.append(damn3)
         buttons.append(full)
         buttons.append(stop)
+        buttons.append(record)
         
         for button in buttons {
             formatButton(button)
         }
-    
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            //try audioSession.overrideOutputAudioPort(AVAudioSessionPortOverride.Speaker)
+            try audioSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try audioRecorder = AVAudioRecorder(URL: self.directoryURL()!,
+                settings: recordSettings)
+            audioRecorder.prepareToRecord()
+        } catch {
+        }
         // Do any additional setup after loading the view, typically from a nib.
         
     }
     
+    func directoryURL() -> NSURL? {
+        let fileManager = NSFileManager.defaultManager()
+        let urls = fileManager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        let documentDirectory = urls[0] as NSURL
+        let soundURL = documentDirectory.URLByAppendingPathComponent("sound.m4a")
+        return soundURL
+    }
+    
     func formatButton(button: UIButton) {
-        //button.frame = CGRectMake(400, 400, 300, 300)
         
         button.tintColor = UIColor.redColor()
         button.layer.cornerRadius = 0.5 * button.bounds.size.width
